36c77e533fc717100137c68a1a0d7453eb7ed097
Sebastian Beier <sebastianbeier@Sebastians-MacBook-Pro.local>
2019-09-19 10:38:32 +0200

    Add Axios and Database Access for initialState and storing total expense and single expenditure. Add Spinner while accessing Database.

--- src/container/MainContent/MainContent.js
+++ src/container/MainContent/MainContent.js
@@ -1,19 +1,93 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Aux from '../../hoc/Aux/Aux';
 import BudgetOutputs from '../../components/BudgetOutputs/BudgetOutputs';
 import SpendingInput from '../../components/SpendingInput/SpendingInput';
+import Spinner from '../../components/UI/Spinner';
 
 import * as helper from '../../helper/helper';
 
 class MainContent extends Component {
-  state = helper.getInitialState();
+  state = {
+    monthlyBudget: 0,
+    totalExpenditure: 0,
+    totalAvailable: 0,
+    dailyAvailable: 0,
+    spendingInputValue: 0,
+    loading: false 
+  };
+
+  componentDidMount() {
+    this.setState({
+      loading: true
+    });
+    
+    axios.get('/expenditure/-LoUWQkmjyAhwPsPHU6l/totalExpenditure.json')
+      .then(result => {
+        const totalExpenditure = +(result.data.totalExpenditure);
+        const monthlyBudget = +(354).toFixed(2);
+        const totalAvailable = helper.calculateTotalAvailable(monthlyBudget, totalExpenditure);
+        const dailyAvailable = helper.calculateDailyAvailable(totalAvailable);
+
+        this.setState({
+          monthlyBudget: monthlyBudget,
+          totalExpenditure: totalExpenditure.toFixed(2),
+          totalAvailable: totalAvailable.toFixed(2),
+          dailyAvailable: dailyAvailable.toFixed(2),
+          spendingInputValue: 0,
+          loading: false
+        })
+      })
+      .catch(error => {
+        console.log(error);
+        this.setState({
+          loading: false
+        })
+      });
+  };
 
   acceptSpendingHandler = () => {
+    this.setState({
+      loading: true
+    })
+
     let expense = this.state.spendingInputValue;
     let newTotalAvailable = helper.calculateTotalAvailable(this.state.totalAvailable, expense);
     let newTotalExpenditure = parseFloat(this.state.totalExpenditure)+parseFloat(expense);
     let newDailyAvailable = helper.calculateDailyAvailable(newTotalAvailable);
+
+    const storageExpenseData = {
+      singleExpense: {
+        date: new Date(),
+        expenseValue: expense
+      }
+    };
+    const storageTotalExpenditureData = {
+      totalExpenditure: newTotalExpenditure.toFixed(2)
+    };
+
+    axios.post('/singleExpenses.json', storageExpenseData)
+    .then(response => console.log(response))
+    .catch(error => {
+      console.log(error);
+      this.setState({
+        loading: false
+      })});
+    axios.put('/expenditure/-LoUWQkmjyAhwPsPHU6l/totalExpenditure.json', storageTotalExpenditureData)
+    .then(response => {
+      console.log(response);
+      this.setState({
+        loading: false
+      })
+    })
+    .catch(error => {
+      console.log(error);
+      this.setState({
+        loading: false
+      })
+    });
+    
     this.setState({
       totalAvailable: newTotalAvailable.toFixed(2),
       totalExpenditure: newTotalExpenditure.toFixed(2),
@@ -28,15 +102,27 @@ class MainContent extends Component {
   };
 
   render () {
-    return(
-      <Aux>
+
+    let mainContent = (
+      <div>
         <BudgetOutputs
-          monthlyBudget={this.state.monthlyBudget}
-          totalAvailable={this.state.totalAvailable}
-          dailyAvailable={this.state.dailyAvailable}/>
+            monthlyBudget={this.state.monthlyBudget}
+            totalAvailable={this.state.totalAvailable}
+            dailyAvailable={this.state.dailyAvailable}/>
         <SpendingInput
           applySpending={this.acceptSpendingHandler}
           inputValue={this.spendingInputValueHandler}/>
+      </div>
+    )
+    if (this.state.loading) {
+      mainContent = (
+          <Spinner />
+      )
+    };
+
+    return(
+      <Aux>
+        {mainContent}
       </Aux>
     )
   }
--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,26 +1,57 @@
 // import monthlyAvailable from '../assets/data/monthlyAvailable';
+import axios from 'axios';
 
-export const getInitialState = () => {
-  let initialData = getData();
-  let totalAvailable = calculateTotalAvailable(initialData.monthlyBudget, initialData.totalExpenditure);
-  let dailyAvailable = calculateDailyAvailable(totalAvailable);
-  return {
-    ...initialData,
-    totalAvailable: totalAvailable.toFixed(2),
-    dailyAvailable: dailyAvailable.toFixed(2),
-    spendingInputValue: 0 
-  }
-};
+// export const getInitialState = () => {
+  // let initialData = getData();
+  // axios.get('/expenditure/-LoUWQkmjyAhwPsPHU6l/totalExpenditure.json')
+  //   .then(result => {
+  //   const totalExpenditure = +(result.data.totalExpenditure);
+  //   const monthlyBudget = +(354).toFixed(2);
+  //   const totalAvailable = calculateTotalAvailable(monthlyBudget, totalExpenditure);
+  //   const dailyAvailable = calculateDailyAvailable(totalAvailable);
+  //   return {
+  //     monthlyBudget: monthlyBudget,
+  //     totalExpenditure: totalExpenditure.toFixed(2),
+  //     totalAvailable: totalAvailable.toFixed(2),
+  //     dailyAvailable: dailyAvailable.toFixed(2),
+  //     spendingInputValue: 0 
+  //   }
+  // });
+  // ;
+  // let totalAvailable = calculateTotalAvailable(initialData.monthlyBudget, initialData.totalExpenditure);
+  // let dailyAvailable = calculateDailyAvailable(totalAvailable);
+  // return {
+  //   ...initialData,
+  //   totalAvailable: totalAvailable.toFixed(2),
+  //   dailyAvailable: dailyAvailable.toFixed(2),
+  //   spendingInputValue: 0 
+  // }
+// };
 
-const getData = () => {
+// const getData = () => {
   // get monthly Budget and total Expenses from Database
   // const actualMonth = new Date().getMonth()+1;
   // const monthlyBudget = monthlyAvailable[actualMonth];
-  return {
-    monthlyBudget: (354).toFixed(2),
-    totalExpenditure: (0).toFixed(2)
-  };
-};
+
+// axios all und spread für monatliches budget und total ausgaben
+
+  // const getTotalExpenditure = () => {
+  //   return axios.get('/expenditure/-LoUWQkmjyAhwPsPHU6l/totalExpenditure.json')};
+
+  // const getTotalExpenditure = () => {
+  //   return axios.get('/expenditure/-LoUWQkmjyAhwPsPHU6l/totalExpenditure.json')};
+  // axios.get('/expenditure/-LoUWQkmjyAhwPsPHU6l/totalExpenditure.json')
+  //   .then(result => {
+  //   const totalExpenditure = +(result.data.totalExpenditure);
+  //   return {
+  //     totalExpenditure: totalExpenditure.toFixed(2),
+  //     monthlyBudget: (354).toFixed(2)
+  //   }});
+  // return {
+  //   monthlyBudget: (354).toFixed(2),
+  //   totalExpenditure: (0).toFixed(2)
+  // };
+// };
 
 export const calculateTotalAvailable = (budget, expenses) => {
   return (budget-expenses);
@@ -43,11 +74,11 @@ export const calculateDailyAvailable = (availableBudget) => {
     }
   };
 
-  const date = new Date();
-  const month = date.getMonth()+1;
-  const year = date.getFullYear();
-  const day = date.getDate();
-  const totalDays = daysOfMonth(month, year);
-  const daysLeft = totalDays-day;
+  let date = new Date();
+  let month = date.getMonth()+1;
+  let year = date.getFullYear();
+  let day = date.getDate();
+  let totalDays = daysOfMonth(month, year);
+  let daysLeft = totalDays-day;
   return availableBudget/daysLeft.toFixed(2);
 };
