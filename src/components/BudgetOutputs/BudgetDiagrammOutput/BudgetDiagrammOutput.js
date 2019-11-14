4811fe164361bfda7b78f5ef5d53aea0af3c31c0
Sebastian Beier <sebastianbeier@Sebastians-MacBook-Pro.local>
2019-11-14 14:28:27 +0100

    Add check for width in budget diagramm over 100 and color font in red if out of budget.

--- src/components/BudgetOutputs/BudgetDiagrammOutput/BudgetDiagrammOutput.js
+++ src/components/BudgetOutputs/BudgetDiagrammOutput/BudgetDiagrammOutput.js
@@ -3,14 +3,25 @@ import React from "react";
 import styles from "../BudgetOutput/BudgetOutput.module.css";
 
 const budgetDiagrammOutput = props => {
-  const width = ((props.usedFromBudget * 100) / props.budget).toFixed(2);
+  let width = ((props.usedFromBudget * 100) / props.budget).toFixed(2);
+  if (width >= 100) {
+    width = 100;
+  }
   const budgetWidth = width.toString() + "%";
 
   return (
     <div className={styles.BudgetOutput}>
       <p>{props.children}</p>
       <p>
-        {props.usedFromBudget} € / {props.budget} €
+        <span
+          style={{
+            color: props.usedFromBudget > props.budget ? "red" : "black",
+            fontWeight: props.usedFromBudget > props.budget ? "bold" : ""
+          }}
+        >
+          {props.usedFromBudget}
+        </span>{" "}
+        € / {props.budget} €
       </p>
       <div className={styles.BudgetBox}>
         <div className={styles.BudgetUsed} style={{ width: budgetWidth }}></div>
